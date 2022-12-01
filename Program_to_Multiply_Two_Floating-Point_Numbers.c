a282d03876578bac95838d05643210fb203c6c42
PRajesh999 <114941231+PRajesh999@users.noreply.github.com>
2022-12-01 11:20:29 +0530

    #include <stdio.h> int main() {     float c,a,b;     scanf("%f%f",&a,&b);     c=a*b;     printf("%.2f",c); }

--- Program_to_Multiply_Two_Floating-Point_Numbers.c
+++ Program_to_Multiply_Two_Floating-Point_Numbers.c
@@ -1,9 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    float a,b,c;
-    scanf("%f %f",&a,&b);
+    float c,a,b;
+    scanf("%f%f",&a,&b);
     c=a*b;
     printf("%.2f",c);
-    
 }
