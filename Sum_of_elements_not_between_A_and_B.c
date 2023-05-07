f6c9b0538f02a350a6bb2c0444f45704f1c881dc
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-05-07 14:38:05 +0530

    #include <stdio.h> int main() { 	int n,i; 	scanf("%d",&n); 	int c[n],sum=0; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&c[i]); 	} 	int a,b; 	scanf("%d %d",&a,&b); 	for(i=0;i<n;i++) 	{ 		if(c[i]<a || c[i]>b ) 		{ 		    sum+=c[i]; 		}
    	}
    	printf("%d",sum);
    	
    }

---
+++ Sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	int c[n],sum=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&c[i]);
+	}
+	int a,b;
+	scanf("%d %d",&a,&b);
+	for(i=0;i<n;i++)
+	{
+		if(c[i]<a || c[i]>b )
+		{
+		    sum+=c[i];
+		}
+		
+	}
+	printf("%d",sum);
+	
+}
+ 
