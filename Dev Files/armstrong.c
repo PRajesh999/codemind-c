52b62667fabeea0b0d5d741fd2dc04c518f64b83
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-05-27 18:20:37 +0530

    Add files via upload

---
+++ Dev Files/armstrong.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int n,i,r,temp,sum=0;
+	scanf("%d",&n);
+	temp=n;
+	while(temp!=0)
+	{
+		r=temp%10;
+		sum=sum+(r*r*r);
+		temp=temp/10;
+	}
+	if(sum==n)
+	{
+		printf("Armstrong");
+	}
+	else
+	{
+		printf("Not");
+	}
+}
