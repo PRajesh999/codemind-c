dfe549c52e5977ade6a11e5d914800a6645e439d
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-05-30 13:34:39 +0530

    Add files via upload

---
+++ Dev Files/calloc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	int *ptr;
+	ptr=(int *)calloc(n,sizeof(int));
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",ptr+i);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d---->  %d\n",ptr+i,*(ptr+i));
+	}
+	free(ptr);
+	for(i=0;i<n;i++)
+	{
+		printf("%d---->  %d\n",ptr+i,*(ptr+i));
+	}
+	
+}
