2a1942b9a2d9ee858e68b1d06bdf030551e7af58
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-05-30 13:33:53 +0530

    Add files via upload

---
+++ Dev Files/callbyvalue swap(fun)(address).c
@@ -0,0 +1,20 @@
+//call by value
+#include <stdio.h>
+void swap(int,int);
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	swap(&a,&b); //function call swap(10,20)
+	printf("%d %d\n",&a,&b);
+	printf("a=%d b=%d",a,b);
+}
+void swap(int *x,int *y) //formal variables
+{
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+	printf("x=%d y=%d\n",x,y);
+}
+
