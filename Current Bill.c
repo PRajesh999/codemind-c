409b079914a72605b9b521e7c85a548844d0c255
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-06-02 10:55:16 +0530

    Add files via upload

---
+++ Current Bill.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    float u,a;
+    scanf("%f",&u);
+    int x;
+    if(u<200)
+    {
+        x=u*1.20;
+    }
+    else if(u>=200&&u<400)
+    {
+        x=u*1.50;
+    }
+    else if(u>=400&&u<600)
+    {
+        x=u*1.80;
+    }
+    else if(u>=600)
+    {
+        x=u*2.0;
+    }
+    if(x>400)
+    {
+        a=x+x*0.15;
+    }
+    else
+    {
+        a=x+100;
+    }
+    printf("%.2f",a);
+}
