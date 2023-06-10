20f8873cace738b0ab89f48cd019700feeb321f8
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-06-10 14:39:51 +0530

    Add files via upload

---
+++ Dev Files/dspointer4 call by value.c
@@ -0,0 +1,14 @@
+//call by value
+#include <stdio.h>
+void modify(int a)   //a=10
+{
+	a=a+1;           
+}
+int main()
+{
+	int a=10;
+	printf("Before modification : %d\n" ,a);
+	modify(a);
+	printf("After modification : %d\n", a);
+	
+}
