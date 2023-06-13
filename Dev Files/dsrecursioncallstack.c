36659546f61525eba60ecb4c9b1462fbfa92a4bd
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-06-13 18:28:52 +0530

    Add files via upload

---
+++ Dev Files/dsrecursioncallstack.c
@@ -0,0 +1,11 @@
+/*
+Call Stack
+
+fact(1)  1                               Each row is called as a stack frame
+fact(2)  2*fact(1)
+fact(3)  3*fact(3)
+fact(4)  4*fact(3)
+fact(5)  5*fact(4)
+main()   fact(5)
+*/
+
