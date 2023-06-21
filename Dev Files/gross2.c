ea8ef5d89432e21a2fc8192c1fe073dfad1f4435
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-06-21 18:43:25 +0530

    Add files via upload

---
+++ Dev Files/gross2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c,d;
+    scanf("%f",&a);
+    if(a<=10000)
+    {
+        b=0.8*a;
+        c=0.2*a;
+    }
+    else if(a<=20000)
+    {
+        b=0.9*a;
+        c=0.25*a;
+    }
+    else if(a>20000)
+    {
+        b=0.95*a;
+        c=0.3*a;
+    }
+    d=a+b+c;
+    printf("%.2f",d);
+}
