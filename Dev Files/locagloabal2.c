470524d1a3bfd11d873ac0bb787179ada769b047
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-06-24 19:09:51 +0530

    Add files via upload

---
+++ Dev Files/locagloabal2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int arr[5];
+void read()
+{
+	int i;
+	for(i=0;i<5;i++) scanf("%d",&arr[i]);
+}
+int main()
+{
+	int i;
+	read();
+	for(i=0;i<5;i++) printf("%d ",arr[i]);
+	return 0;
+}
